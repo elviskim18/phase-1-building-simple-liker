bf8c0a358ebe34cdfb65dd4aa5598d426cd4359b
Kimani <elvis.kimani@student.moringaschool.com>
2022-06-16 17:47:53 +0300

    Done.

--- main.js
+++ main.js
@@ -5,6 +5,65 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+//add class hidden to modal
+const errorModal = document.getElementById('modal')
+errorModal.className = "hidden"
+
+//get all like buttons
+const likeBtn = document.querySelectorAll('.like-glyph');
+
+
+
+//create function that is called by event listener 
+function likepost (event){
+  const heart = event.target;
+  heart.innerHTML = EMPTY_HEART;
+
+  //invoke mimicServerCall
+  mimicServerCall()
+
+  //if the servercall is succesfull change heart to full heart
+  .then(response => {
+    if(heart.innerHTML === EMPTY_HEART){
+      heart.innerHTML == FULL_HEART;
+      heart.classList.add("activated-heart")
+    }
+    else {
+      heart.innerHTML == EMPTY_HEART;
+      heart.classList.remove("activated-heart")
+    }
+  })
+
+  //error handler
+  .catch((error) => {
+    errorModal.classList.remove("hidden");
+    document.getElementById("modal-message").textContent = error
+    //add
+    setTimeout(() =>{
+      errorModal.classList.add("hidden")
+    },3000)
+  })
+
+
+
+
+}
+
+
+//add eventlistener to likeBtn
+likeBtn.forEach(heart => {
+  heart.addEventlistener("click",likepost)
+  
+});
+
+
+
+
+
+
+
+
+
 
 
 //------------------------------------------------------------------------------
